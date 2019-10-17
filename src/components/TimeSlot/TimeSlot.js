ac9bae45f3f96d8f07c16e5cc788ef9456420fea
Ethan Zimmerman <thebinarypenguin@gmail.com>
2019-10-17 14:22:10 -0400

    Break render into 2 seperate functions

--- src/components/TimeSlot/TimeSlot.js
+++ src/components/TimeSlot/TimeSlot.js
@@ -2,16 +2,33 @@ import React from 'react';
 
 function TimeSlot(props) {
 
+  let suffix = (props.hour > 11) ? 'pm' : 'am';
   let time = (props.hour > 12) ? props.hour - 12 : props.hour;
 
-  if (time > 11) { time += ' pm' }
-  else { time += ' am' }
+  const renderEmpty = () => {
 
-  return (
-    <div className="TimeSlot" onClick={props.onClick}>
-      <strong>{time}</strong> - {props.appointment && props.appointment.name} {props.appointment && props.appointment.phoneNumber}
-    </div>
-  );
+    return (
+      <div className="TimeSlot list-group-item list-group-item-success" onClick={props.onClick}>
+        <strong>{time + suffix}</strong>
+      </div>
+    );
+  };
+
+  const renderFull = () => {
+
+    return (
+
+      <div className="TimeSlot list-group-item list-group-item-danger" onClick={props.onClick}>
+        <strong>{time + suffix}</strong> - {props.appointment.name} {props.appointment.phoneNumber}
+      </div>
+    )
+  }
+
+  if (props.appointment) {
+    return renderFull();
+  }
+
+  return renderEmpty();
 }
 
 export default TimeSlot;
