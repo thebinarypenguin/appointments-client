980ddd99084f1047015226dc551b31acfb50f43d
Ethan Zimmerman <thebinarypenguin@gmail.com>
2019-10-17 12:06:52 -0400

    Create EditTimeSlotModal Stub

---
+++ src/components/EditTimeSlotModal/EditTimeSlotModal.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function EditTimeSlotModal() {
+
+  return (
+    <div className="EditTimeSlotModal">
+      <h1>EditTimeSlotModal</h1>
+    </div>
+  );
+}
+
+export default EditTimeSlotModal;
